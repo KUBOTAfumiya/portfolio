6b619629cd985f9add49ca91ef02e1a86efb77fd
KUBOTA Fumiya <two.three.eight@gmail.com>
2020-08-19 16:20:53 +0900

    add Navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <nuxt-link to="/" class="navbar-item">TWO.THREE.EIGHT</nuxt-link>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <nuxt-link to="/" class="navbar-item">
+          Home
+        </nuxt-link>
+
+        <nuxt-link to="/about" class="navbar-item">
+          About
+        </nuxt-link>
+
+        <nuxt-link to="/about" class="navbar-item">
+          Work
+        </nuxt-link>
+
+        <nuxt-link to="/about" class="navbar-item">
+          Career
+        </nuxt-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
